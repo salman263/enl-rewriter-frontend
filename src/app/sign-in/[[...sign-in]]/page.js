42e69d7abc870e18428213549d8c76f45563e215
Salman Mondal <salman45.sm@gmail.com>
2026-08-26 15:57:51 +0530

    Fixed Clerk white screen issue by adding force redirect URLs

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -3,7 +3,13 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#fafbfe] selection:bg-[#03d665] selection:text-white">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      <SignIn 
+        path="/sign-in" 
+        routing="path" 
+        signUpUrl="/sign-up" 
+        fallbackRedirectUrl="/dashboard"
+        forceRedirectUrl="/dashboard"
+      />
     </div>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -3,7 +3,13 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#fafbfe] selection:bg-[#03d665] selection:text-white">
-      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+      <SignUp 
+        path="/sign-up" 
+        routing="path" 
+        signInUrl="/sign-in" 
+        fallbackRedirectUrl="/dashboard"
+        forceRedirectUrl="/dashboard"
+      />
     </div>
   );
 }
